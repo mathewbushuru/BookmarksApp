521aed265fe8ffc98a6fed81951f8d2448f3f2e9
Mathew Bushuru <bushuruwafulamathew@gmail.com>
2022-07-11 11:05:56 -0700

    webview

--- App.js
+++ App.js
@@ -1,5 +1,6 @@
 import { StyleSheet, Text, View } from "react-native";
 import React from "react";
+import {WebView} from "react-native-webview"
 
 import Welcome from "./components/Welcome";
 
@@ -7,13 +8,14 @@ export default function App() {
   const names = ["Mathew", "Mark", "Glo"];
 
   return (
-    <View style={styles.container}>
-      <Text>Bookmarks App</Text>
+    <WebView source={{uri:"https://www.reddit.com"}}/>
+    // <View style={styles.container}>
+    //   <Text>Bookmarks App</Text>
 
-      {names.map((name) => (
-        <Welcome name={name} key={name} />
-      ))}
-    </View>
+    //   {names.map((name) => (
+    //     <Welcome name={name} key={name} />
+    //   ))}
+    // </View>
   );
 }
 
